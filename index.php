a423c981e46c8cd6e459ca5b885c31960852e9b3
WalterFrongia <walter.frongia@libero.it>
2021-10-13 17:13:15 +0200

    Implementazione array e stampa con foreach

--- index.php
+++ index.php
@@ -4,19 +4,72 @@ Occupatevi di gestire la visualizzazione dei dati in PHP -->
 
 <?php
 
-    $faqGqoogle = [
-        question_1 => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
-        answer_1 => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
-
-        Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.
-        
-        Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.
-        
-        Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
-        
-        Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.",
-    ];   
+    $faqGoogle = [
+        [
+            "domanda" => "domanda 1",
+            "risposta"=> "risposta 1",
+        ],
+        [
+            "domanda" => "domanda 2",
+            "risposta"=> "risposta 2",
+        ],
+        [
+            "domanda" => "domanda 3",
+            "risposta"=> "risposta 3",
+        ],
+        [
+            "domanda" => "domanda 4",
+            "risposta"=> "risposta 4",
+        ],
+        [
+            "domanda" => "domanda 5",
+            "risposta"=> "risposta 5",
+        ],
+        [
+            "domanda" => "domanda 6",
+            "risposta"=> "risposta 6",
+        ],
+    ];
+            
 
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+    <!--link bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <!--Link foglio di stile css-->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Php google faq</title>
+</head>
+<body>
+
+    <h1>Google Faq</h1>
+
+    <div id="my-content">
+        
+        <?php
+        foreach($faqGoogle as $chiave => $valore ){
+                // echo $chiave;
+                // echo $valore;
+                // var_dump($valore);
+            ?>
+            <h3> <?php echo $valore["domanda"]; ?> </h3>
+            <p><?php echo $valore["risposta"]; ?> </p>
+        <?php } ?>
+
+    </div>
+
+</body>
+</html>
